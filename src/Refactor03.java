f8cbc57ae85947295679be6640932bd2c0ff5642
ALUMNADO\maruan <maruan@floridauniversitaria.es>
2016-11-17 19:47:49 +0100

    Refactorizado!

--- src/Refactor03.java
+++ src/Refactor03.java
@@ -1,31 +1,69 @@
 
 public class Refactor03 {
 
+	//Calculamos el movimiento de un peón en un tablero de ajedrez
+	//Para una partida con más peones
+	//Juegan negras en la parte inferior del tablero
+	private static int colPeonNegra;
+	private static int filPeonNegra;
+	private static int colPeonBlanca;
+	private static int filPeonBlanca;
+	private static int sigmovColPeonNegra;
+	private static int sigmovFilPeonNegra;
+	
+	public Refactor03(){
+		colPeonNegra=1;
+		filPeonNegra=3;
+		colPeonBlanca=1;
+		filPeonBlanca=3;
+	}
+	
+	
 	public static void main(String[] args) {
-		//Calculamos el movimiento de un peón en un tablero de ajedrez
-		//Para una partida con más peones
-		//Juegan negras en la parte inferior del tablero
-		int colPeonNegra=1;
-		int filPeonNegra=3;
-		int colPeonBlanca=1;
-		int filPeonBlanca=3;
-		int sigmovColPeonNegra;
-		int sigmovFilPeonNegra;
+		
 		
 		//Calculamos siguiente movimieto
-		if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra++==filPeonBlanca)){
+		if(PodemosMoverDiagDerPeonBlanco()) {// colPeonNegra++==colPeonBlanca)&&(filPeonNegra++==filPeonBlanca)){
 			//Me como blanca colocado en diagonal derecha
-			sigmovColPeonNegra=colPeonNegra++;
-			sigmovColPeonNegra=filPeonNegra++;
-		}else if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra--==filPeonBlanca)){
+			MovemosDiagDerPeonBlanco();
+		}else if(PodemosMoverDiagIzqPeonNegra()){ //(colPeonNegra++==colPeonBlanca)&&(filPeonNegra--==filPeonBlanca)
 			//Este movimiento está mal
 			//Me como blanca colocado en diagonal izda
-			sigmovColPeonNegra=colPeonNegra++;
-			sigmovColPeonNegra=filPeonNegra--;
-		}else if((colPeonNegra++!=colPeonBlanca)){
+			MovemosDiagIzqPeonNegra();
+		}else if(PodemosMoverAdelantePeonNegra()){ //(colPeonNegra++!=colPeonBlanca)){
 			//Muevo adelante
-			sigmovColPeonNegra=colPeonNegra++;
+			MuevoAdelantePeonNegra();
 		}
 	}
-
+	
+	
+	
+	public static boolean PodemosMoverDiagDerPeonBlanco() {
+		return ((colPeonNegra++==colPeonBlanca)&&(filPeonNegra++==filPeonBlanca))?true:false;
+	}
+	
+	public static boolean PodemosMoverDiagIzqPeonNegra(){
+		return (colPeonNegra++==colPeonBlanca)&&(filPeonNegra--==filPeonBlanca)?true:false;
+	}
+	
+	public static boolean PodemosMoverAdelantePeonNegra(){
+		return (colPeonNegra++!=colPeonBlanca)?true:false;
+	}
+		
+	public static void MovemosDiagDerPeonBlanco(){
+		sigmovColPeonNegra=colPeonNegra++;
+		sigmovColPeonNegra=filPeonNegra++;
+	}
+	
+	public static void MovemosDiagIzqPeonNegra(){
+		sigmovColPeonNegra=colPeonNegra++;
+		sigmovColPeonNegra=filPeonNegra--;
+	}
+	
+	public static void MuevoAdelantePeonNegra(){
+		sigmovColPeonNegra=colPeonNegra++;
+	}
 }
+
+
+
